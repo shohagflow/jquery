5f59381a854c73471c6b6f58332f19cb54c3685b
Shohag <shohagflow@gmail.com>
2025-06-10 14:57:24 +0600

    Initial commit of class8

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$('button').dblclick(function(){
+    alert('Doduel Click');
+});
+
+$('.one').mouseenter(function(){
+    alert('hello Shohag');
+});
+
+$('.one').mouseleave(function(){
+    alert('Mouse Leave ');
+});
+
+});
